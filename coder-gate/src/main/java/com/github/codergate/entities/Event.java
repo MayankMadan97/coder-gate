9ce97954be1f3ad9ceab5610cbf98580449ae962
Aadith Shameel <aadith@Aadiths-MBP.hitronhub.home>
2023-02-21 22:49:36 -0400

    Added Event and Analysis Entities. Renamed UserRepository Interface to WebhookListenerRepository

--- coder-gate/src/main/java/com/github/codergate/entities/Event.java
+++ coder-gate/src/main/java/com/github/codergate/entities/Event.java
@@ -1,4 +1,31 @@
 package com.github.codergate.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Event")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+
+    @Id
+    private int eventID;
+    private String eventName;
+    private int commitID;
+    private String commitMessage;
+    @ManyToOne
+    @JoinColumn(name = "userID")
+    private User u2;
+
+    @ManyToOne
+    @JoinColumn(name = "repositoryID")
+    private Repository r2;
+
 }
--- coder-gate/src/main/java/com/github/codergate/repositories/WebhookListenerRepository.java
+++ coder-gate/src/main/java/com/github/codergate/repositories/WebhookListenerRepository.java
@@ -1,9 +1,9 @@
 package com.github.codergate.repositories;
 
-import com.github.codergate.entities.User;
+import com.github.codergate.entities.Analysis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface WebhookListenerRepository extends JpaRepository<Analysis, Integer> {
 }
