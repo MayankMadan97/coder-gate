05516c071d9b285462faf11987b78659debc40a5
Mayank Madan <my905874@dal.ca>
2023-03-12 13:08:11 -0300

    Added Jacoco integration for test coverage report generation

--- coder-gate/src/test/java/com/github/codergate/CoderGateApplicationTests.java
+++ coder-gate/src/test/java/com/github/codergate/CoderGateApplicationTests.java
@@ -1,13 +1,8 @@
 package com.github.codergate;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoderGateApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
