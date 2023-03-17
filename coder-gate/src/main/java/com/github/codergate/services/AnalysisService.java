d169d14e8cebd5b5af9bceafa585d5588f11054a
Taranjot Singh <taranjot20@gmail.com>
2023-03-17 10:40:48 -0300

    Created processAnalysis method

--- coder-gate/src/main/java/com/github/codergate/services/AnalysisService.java
+++ coder-gate/src/main/java/com/github/codergate/services/AnalysisService.java
@@ -1,12 +1,31 @@
 package com.github.codergate.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.codergate.controllers.AnalysisController;
+import com.github.codergate.dto.installation.InstallationPayloadDTO;
 import com.github.codergate.entities.AnalysisEntity;
 import com.github.codergate.repositories.AnalysisRepository;
+import com.github.codergate.utils.Mapper;
+import org.json.JSONObject;
+import org.json.XML;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
 
 @Service
 public class AnalysisService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnalysisService.class);
     AnalysisRepository analysisRepository;
 
     public void createAnalysis() {
@@ -20,5 +39,15 @@ public class AnalysisService {
 
     public void convertEntityToDto(AnalysisEntity analysisEntity) {
 
+    }
+
+    public void processAnalysis(MultipartFile file) throws Exception {
+        try (InputStream inputStream = file.getInputStream()) {
+            byte[] bytes = inputStream.readAllBytes();
+            String xml = new String(bytes);
+            JSONObject jsonObject = XML.toJSONObject(xml);
+            LinkedHashMap<String, Object> analysisPayload = new ObjectMapper()
+                    .readValue(jsonObject.toString(), LinkedHashMap.class);
+        }
     }
 }
