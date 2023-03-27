d742d3687fe425cab8a892ef04c10241ac0d963b
Taranjot Singh <taranjot20@gmail.com>
2023-03-27 12:07:33 -0300

    Created the threshold form

--- coder-gate-ui/src/app/main/dashboard/dashboard.component.ts
+++ coder-gate-ui/src/app/main/dashboard/dashboard.component.ts
@@ -1,34 +1,38 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  myForm?: FormGroup;
+  //bugs: AbstractControl | undefined;
+
+  constructor(private fb: FormBuilder) {}
+  
   public selectedRepo?: string;
   public showThresholdView = false;
 
-  myForm?: FormGroup;
-
-
-  constructor(private fb: FormBuilder) {
-  }
-
   ngOnInit() {
     this.myForm = this.fb.group({
-      repository: ['', Validators.required],
-      smellDensity: ['', [Validators.required]],
-      message: ['', Validators.required]
+      bugs: ['', Validators.required],
+      vulnerabilities: ['', Validators.required],
+      codeSmell: ['', Validators.required],
+      testCoverage: ['', Validators.required],
+      duplicatedLines: ['', Validators.required],
+      cyclomaticComplexitySlider: ['', Validators.required],
+      documentedLines: ['', Validators.required],
     });
   }
 
   onSubmit() {
     if (this.myForm?.valid) {
       // Do something with the form data here
-      console.log(this.myForm.value);
+      //const formValues = this.myForm.value;
+      console.warn(this.myForm.value);
     }
   }
 
