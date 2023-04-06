3ed026719d867535b7962a6f6b47e5495c014040
Taranjot Singh <taranjot20@gmail.com>
2023-04-05 22:34:12 -0300

    Added test cases for Mapper & JwtUtils classes

--- coder-gate/src/test/java/com/github/codergate/utils/JwtUtilsTest.java
+++ coder-gate/src/test/java/com/github/codergate/utils/JwtUtilsTest.java
@@ -3,11 +3,22 @@ package com.github.codergate.utils;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.MultiValueMap;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class JwtUtilsTest {
+    @Test
+    public void testGenerateJwtToken() {
+        String appid = "296934";
+        String expectedToken = JwtUtils.generateJwtToken(appid);
+        assertNotNull(expectedToken);
+        assertNotEquals(expectedToken, "anonymous");
+    }
 
     @Test
     public void testGetGithubSpecificHeaders() {
@@ -16,5 +27,18 @@ public class JwtUtilsTest {
         assertEquals(1, headers.size());
         assertTrue(headers.containsKey("Accept"));
         assertEquals("application/vnd.github+json", headers.getFirst("Accept"));
+    }
+
+    @Test
+    public void testReadPrivateKey() throws IOException {
+        File tmpFile = File.createTempFile("test", ".txt");
+        tmpFile.deleteOnExit();
+        String privateKey = "-----BEGIN RSA PRIVATE KEY-----\n" +
+                "Test private key\n" +
+                "-----END RSA PRIVATE KEY-----\n";
+        Files.write(tmpFile.toPath(), privateKey.getBytes());
+        String expected = "Test private key";
+        String actual = JwtUtils.getPrivateKeyContent(tmpFile);
+        assertEquals(expected, actual.trim());
     }
 }
